9e3b28467308940462ac33e7c21ca0dfeca2cf17
David Chang <dvchangyuan@gmail.com>
2023-12-15 15:48:48 -0800

    initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/Pods-videoUI2_Example/Pods-videoUI2_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_videoUI2_Example : NSObject
+@end
+@implementation PodsDummy_Pods_videoUI2_Example
+@end
---
+++ Example/Pods/Target Support Files/videoUI2/videoUI2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_videoUI2 : NSObject
+@end
+@implementation PodsDummy_videoUI2
+@end
